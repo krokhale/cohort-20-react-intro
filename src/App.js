b1666c0c36f26306cfda53ab155f49fb40b80892
krokhale <krishna.rokhale@gmail.com>
2022-02-03 17:48:59 -0800

    checking in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
 
@@ -8,8 +8,28 @@ function App() {
   // useEffect - Hooks
 
   const [counter, setCounter] = useState(0);
+  const [counterTotal, setCounterTotal] = useState(0);
+  const [todoText, setTodoText] = useState('');
+  const [list, setList] = useState([]);
+  const [name, setName] = useState({});
+
+  // {firstName: 'Jane', lastName: 'Doe'}
+  // setName({...name, middleName: 'something'})
 
   // let counter = 0
+
+  useEffect(() => {
+    console.log('run this code when the page loads ONLY ONCE')
+  }, [])
+
+  useEffect(() => {
+    console.log('Run this code anytime any of the states change in the component')
+  })
+
+  useEffect(() => {
+    console.log('Run this code anytime the counter state variable changes')
+    setCounterTotal(counter + counterTotal)
+  }, [counter])
 
   const onButtonClick = () => {
     console.log('the button was clicked!')
@@ -17,7 +37,20 @@ function App() {
     // let newCounter = counter + 1
     // setCounter(newCounter)
     setCounter(counter + 1)
+    // setCounterTotal(counter + counterTotal)
     // document.getElementById('counter-div-result').innerText = counter
+  };
+
+  const onInputChange = (event) => {
+    console.log(event.currentTarget.value)
+    setTodoText(event.currentTarget.value)
+
+  };
+
+  const submitItem = () => {
+    // list.push(event.currentTarget.value) // Would mean that you are manipulating the list state variable directly.
+    setList([...list, todoText])
+    setTodoText('')
   };
 
 
@@ -30,6 +63,8 @@ function App() {
           <h2 style={counter==6 ? {color: 'red'} : {color: 'green'}}>The counter value is {counter}</h2>
           <h2 className={counter==7 ? 'something-7': 'something'}>The counter value is {counter}</h2>
 
+          <h2>The counter Total is: {counterTotal}</h2>
+
           {/*Ternary Operators / Conditionals*/}
 
           {(counter==5 || counter==10) && <h1>We are at 5 OR 10, yay!</h1>}
@@ -37,6 +72,23 @@ function App() {
           {counter==5 && <h1>We are at 5, yay!</h1>}
 
           {counter==10 && <h1>We are at 10, yay!</h1>}
+
+          <h1>The current list is: {JSON.stringify(list)}</h1>
+
+
+          <input value={todoText} onChange={onInputChange} type="text"/>&nbsp;
+
+          <button onClick={submitItem}>Submit</button>
+
+          <div style={{marginTop: '20px'}}>
+            <h1 style={{fontSize: '20px'}}>The List Items</h1>
+            <ul>
+              {list && list.map((listItem, idx) => {
+                return <li key={idx}>{listItem}</li>
+              })}
+            </ul>
+
+          </div>
 
 
 
